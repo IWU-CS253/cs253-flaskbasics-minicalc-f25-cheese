dfc6478c7bcb67b39057fa315f4bf17e42978630
Ethan Kuziela <ekuziela@iwu.edu>
2025-09-26 15:43:54 -0500

    c
    alc stuff

--- wordstats.py
+++ wordstats.py
@@ -5,3 +5,16 @@ def average_length(user_string):
     else:
         avg = 0
     return avg
+
+def reverse_string(user_string):
+    reversed_string = user_string[::-1]
+    return reversed_string
+
+def word_counter(user_string):
+    return len(user_string.split())
+
+def total_characters(user_string):
+    character_count = 0
+    for word in user_string.split():
+        character_count += len(word)
+    return character_count
